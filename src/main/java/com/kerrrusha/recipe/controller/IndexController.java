6332040c5b868384e78e7a6fef3bd1c5f6924159
Kirill Koval <brcyrill2@gmail.com>
2023-06-27 02:55:33 +0300

    added recipes model param handling

--- src/main/java/com/kerrrusha/recipe/controller/IndexController.java
+++ src/main/java/com/kerrrusha/recipe/controller/IndexController.java
@@ -1,16 +1,23 @@
 package com.kerrrusha.recipe.controller;
 
+import com.kerrrusha.recipe.service.RecipeService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
+    private final RecipeService recipeService;
+
     @GetMapping({"", "/", "index", "index.html"})
-    public String index() {
+    public String index(Model model) {
         log.info("GET request on index page");
+        model.addAttribute("recipes", recipeService.findAll());
         return "index";
     }
 

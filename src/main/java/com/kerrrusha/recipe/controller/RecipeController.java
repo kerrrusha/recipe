649d8794b64a78253bdaaa14483121a17e6df83d
Kirill Koval <brcyrill2@gmail.com>
2023-07-05 23:46:02 +0300

    added RecipeController

---
+++ src/main/java/com/kerrrusha/recipe/controller/RecipeController.java
@@ -0,0 +1,24 @@
+package com.kerrrusha.recipe.controller;
+
+import com.kerrrusha.recipe.service.RecipeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/recipe")
+@RequiredArgsConstructor
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    @GetMapping("/recipe/show/{id}")
+    public String showById(@PathVariable Long id, Model model){
+        model.addAttribute("recipe", recipeService.findById(id));
+        return "recipe/show";
+    }
+
+}

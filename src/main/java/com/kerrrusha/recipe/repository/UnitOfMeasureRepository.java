a00957deacaf7322d8eee255510ff4f6f6568e2e
Kirill Koval <brcyrill2@gmail.com>
2023-07-05 23:50:09 +0300

    added new method

--- src/main/java/com/kerrrusha/recipe/repository/UnitOfMeasureRepository.java
+++ src/main/java/com/kerrrusha/recipe/repository/UnitOfMeasureRepository.java
@@ -3,5 +3,10 @@ package com.kerrrusha.recipe.repository;
 import com.kerrrusha.recipe.model.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByName(String name);
+
 }

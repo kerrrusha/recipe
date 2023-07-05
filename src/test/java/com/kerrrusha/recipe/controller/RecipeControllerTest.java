b7397ae18cc205e070f9e8b0342f05cf3e0a8c81
Kirill Koval <brcyrill2@gmail.com>
2023-07-05 23:56:34 +0300

    added TDD test

---
+++ src/test/java/com/kerrrusha/recipe/controller/RecipeControllerTest.java
@@ -0,0 +1,40 @@
+package com.kerrrusha.recipe.controller;
+
+import com.kerrrusha.recipe.model.Recipe;
+import com.kerrrusha.recipe.service.RecipeService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(MockitoExtension.class)
+class RecipeControllerTest {
+
+    @Mock
+    RecipeService service;
+
+    @InjectMocks
+    RecipeController controller;
+
+    @Test
+    void showById() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        Recipe recipe = Recipe.builder().id(1L).build();
+        when(service.findById(anyLong())).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+    }
+
+}

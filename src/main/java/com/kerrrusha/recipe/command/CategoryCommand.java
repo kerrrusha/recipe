c2a690995a7e19f6a61fcd2b580b406f6bb3c768
Kirill Koval <brcyrill2@gmail.com>
2023-07-08 01:06:41 +0300

    added toString methods

--- src/main/java/com/kerrrusha/recipe/command/CategoryCommand.java
+++ src/main/java/com/kerrrusha/recipe/command/CategoryCommand.java
@@ -3,9 +3,11 @@ package com.kerrrusha.recipe.command;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
+@ToString
 @NoArgsConstructor
 public class CategoryCommand {
 
--- src/main/java/com/kerrrusha/recipe/command/IngredientCommand.java
+++ src/main/java/com/kerrrusha/recipe/command/IngredientCommand.java
@@ -3,11 +3,13 @@ package com.kerrrusha.recipe.command;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.math.BigDecimal;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 public class IngredientCommand {
 

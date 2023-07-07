1d84051d89535a34f4a2b6cd7d571fd109af0c6e
Kirill Koval <brcyrill2@gmail.com>
2023-07-08 01:06:34 +0300

    added IngredientController

---
+++ src/main/java/com/kerrrusha/recipe/controller/IngredientController.java
@@ -0,0 +1,30 @@
+package com.kerrrusha.recipe.controller;
+
+import com.kerrrusha.recipe.service.RecipeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/recipe")
+public class IngredientController {
+
+    private final RecipeService recipeService;
+
+    @GetMapping("/{recipeId}/ingredients")
+    public String getIngredients(@PathVariable Long recipeId, Model model){
+        log.debug("Getting ingredient list for recipe id: " + recipeId);
+
+        // use command object to avoid lazy load errors in Thymeleaf.
+        model.addAttribute("recipe", recipeService.findCommandById(recipeId));
+
+        return "recipe/ingredient/list";
+    }
+
+}

091f35a04c6693ce201db8c5d4292f0cd6847126
Kirill Koval <brcyrill2@gmail.com>
2023-07-08 01:48:20 +0300

    added get ingredient endpoint

--- src/main/java/com/kerrrusha/recipe/controller/IngredientController.java
+++ src/main/java/com/kerrrusha/recipe/controller/IngredientController.java
@@ -1,5 +1,6 @@
 package com.kerrrusha.recipe.controller;
 
+import com.kerrrusha.recipe.service.IngredientService;
 import com.kerrrusha.recipe.service.RecipeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,15 +17,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IngredientController {
 
     private final RecipeService recipeService;
+    private final IngredientService ingredientService;
 
     @GetMapping("/{recipeId}/ingredients")
-    public String getIngredients(@PathVariable Long recipeId, Model model){
+    public String getRecipeIngredients(@PathVariable Long recipeId, Model model){
         log.debug("Getting ingredient list for recipe id: " + recipeId);
 
         // use command object to avoid lazy load errors in Thymeleaf.
         model.addAttribute("recipe", recipeService.findCommandById(recipeId));
 
         return "recipe/ingredient/list";
+    }
+
+    @GetMapping("/{recipeId}/ingredient/{id}/show")
+    public String get(@PathVariable Long recipeId, @PathVariable Long id, Model model) {
+        model.addAttribute("ingredient", ingredientService.findCommandById(id));
+        return "recipe/ingredient/show";
     }
 
 }

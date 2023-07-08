da96dae0a413a3349958ca6eba0f5dc50a109c6d
Kirill Koval <brcyrill2@gmail.com>
2023-07-08 17:58:26 +0300

    added toString

--- src/main/java/com/kerrrusha/recipe/command/UnitOfMeasureCommand.java
+++ src/main/java/com/kerrrusha/recipe/command/UnitOfMeasureCommand.java
@@ -3,9 +3,11 @@ package com.kerrrusha.recipe.command;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 public class UnitOfMeasureCommand {
 

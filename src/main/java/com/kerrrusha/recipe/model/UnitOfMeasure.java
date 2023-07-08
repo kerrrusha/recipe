ddac51e6d2e6203a90bf743cfec336bf904b778d
Kirill Koval <brcyrill2@gmail.com>
2023-07-08 17:58:34 +0300

    added @SuperBuilder

--- src/main/java/com/kerrrusha/recipe/model/UnitOfMeasure.java
+++ src/main/java/com/kerrrusha/recipe/model/UnitOfMeasure.java
@@ -1,13 +1,19 @@
 package com.kerrrusha.recipe.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
 
 @Entity
 @Getter
 @Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UnitOfMeasure extends BaseEntity {
 
     private String name;

cb372b424e481a300537b7e5cec1c85bcaa053cc
Kirill Koval <brcyrill2@gmail.com>
2023-07-08 17:58:11 +0300

    added update and delete endpoints

--- src/main/java/com/kerrrusha/recipe/controller/IngredientController.java
+++ src/main/java/com/kerrrusha/recipe/controller/IngredientController.java
@@ -1,14 +1,14 @@
 package com.kerrrusha.recipe.controller;
 
+import com.kerrrusha.recipe.command.IngredientCommand;
 import com.kerrrusha.recipe.service.IngredientService;
 import com.kerrrusha.recipe.service.RecipeService;
+import com.kerrrusha.recipe.service.UnitOfMeasureService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
@@ -18,6 +18,7 @@ public class IngredientController {
 
     private final RecipeService recipeService;
     private final IngredientService ingredientService;
+    private final UnitOfMeasureService unitOfMeasureService;
 
     @GetMapping("/{recipeId}/ingredients")
     public String getRecipeIngredients(@PathVariable Long recipeId, Model model){
@@ -33,6 +34,30 @@ public class IngredientController {
     public String get(@PathVariable Long recipeId, @PathVariable Long id, Model model) {
         model.addAttribute("ingredient", ingredientService.findCommandById(id));
         return "recipe/ingredient/show";
+    }
+
+    @GetMapping("/{recipeId}/ingredient/{id}/update")
+    public String update(@PathVariable String recipeId, @PathVariable Long id, Model model) {
+        model.addAttribute("ingredient", ingredientService.findCommandById(id));
+        model.addAttribute("uomList", unitOfMeasureService.findAllCommands());
+        return "recipe/ingredient/create-or-update";
+    }
+
+    @PostMapping("/{recipeId}/ingredient")
+    public String saveOrUpdate(@ModelAttribute IngredientCommand command) {
+        IngredientCommand savedCommand = ingredientService.saveIngredientCommand(command);
+
+        log.debug("Saved recipe id: " + savedCommand.getRecipeId());
+        log.debug("Saved ingredient id :" + savedCommand.getId());
+
+        return "redirect:/recipe/" + savedCommand.getRecipeId() + "/ingredient/" + savedCommand.getId() + "/show";
+    }
+
+    @GetMapping("/{recipeId}/ingredient/{id}/delete")
+    public String delete(@PathVariable String recipeId, @PathVariable Long id) {
+        log.debug("Deleting ingredient with id = {}", id);
+        ingredientService.deleteById(id);
+        return "redirect:/";
     }
 
 }
